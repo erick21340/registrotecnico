36add7bbecb39c123030ef7313fd0c4cac587026
Erick20247 <Torot0086@gmail.vom>
2024-05-11 16:40:55 -0400

    crear el models

---
+++ registrotecnico/registrotecnico/Models/tecnicos.cs
@@ -0,0 +1,20 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace registrotecnico.Models
+{
+    public class tecnicos
+    {
+        [Key]
+        public int tecnicosId { get; set; }
+
+        [Required(ErrorMessage = "Este campo es obligatorio.")]
+        public string? nombres { get; set; }
+
+        
+        public float sueldohoras { get; set; }
+
+       
+
+
+    }
+}
