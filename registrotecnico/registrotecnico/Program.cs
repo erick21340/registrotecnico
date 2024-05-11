288b2d3ac02f8a0947373f11fbbfea0767dbd2a6
Erick20247 <Torot0086@gmail.vom>
2024-05-11 16:52:34 -0400

    INYECTAR EL CONTEXTO

--- registrotecnico/registrotecnico/Program.cs
+++ registrotecnico/registrotecnico/Program.cs
@@ -1,10 +1,17 @@
+using Microsoft.EntityFrameworkCore;
 using registrotecnico.Components;
+using registrotecnico.DAL;
 
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 builder.Services.AddRazorComponents()
     .AddInteractiveServerComponents();
+
+var ConStr = builder.Configuration.GetConnectionString("ConStr");
+builder.Services.AddDbContext<Contexto>(options => options.UseSqlite(ConStr));
+
+
 
 var app = builder.Build();
 
